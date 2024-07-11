0f206b4286833e28713b2eef71000dcac783c047
DebabrataBanik <debabratabanik02@gmail.com>
2024-07-11 19:24:55 +0530

    email validation

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ const buttons = document.querySelectorAll('.btn')
 
 const validateEmail = (target) => {
   const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  let el = target.parentElement.firstElementChild
+  const parentEl = target.parentElement
+  let el = parentEl.querySelector('input[type="text"]');
   if(el.value !== '' && !el.value.match(validRegex)){
-    alert(`Invalid email address`)
+    el.classList.add('invalid')
+    el.nextElementSibling.classList.add('display')
+  }
+  else if(el.value.match(validRegex)){
+    el.classList.remove('invalid')
+    el.nextElementSibling.classList.remove('display')
   }
 
 }
